22e54d57a0fcc310103c31cd8e5d85b830427e51
REHABXR\rehab <hive.arts@ubc.ca>
2024-06-07 11:10:06 -0700

    next build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    basePath: "/nextvanvr",
+    output: "export",
+    reactStrictMode: true,
+  };
+  
 export default nextConfig;
